25b58d1ea8f975f54a46068a1b1308dbca65499f
Enola8847 <ice9393th@gmail.com>
2025-01-28 12:14:34 +0700

    add about me

--- src/App.jsx
+++ src/App.jsx
@@ -14,9 +14,9 @@ function App() {
       <hr className="border border-gray-600 w-full"></hr>
       <footer className="bg-[#011627]  mx-1 h-[125px] p-2 mt-1 mb-2 justify-center items-center">
         <section className="footer grid grid-cols-3 gap-4 text-center">
-          <div className="border-2 border-[#e71d36] p-2  ">
+          <div className="border-2 border-[#e71d36] p-2 h-[100px] md:h-[111px] ">
             <div className="mb-2">
-              <h1 className="text-xl md:text-2xl">My Github</h1>
+              <h1 className="text-md md:text-2xl">My Github</h1>
             </div>
             <div className="flex justify-center items-center">
               <img
@@ -27,17 +27,17 @@ function App() {
               ></img>
             </div>
           </div>
-          <div className="p-4 border-[#e71d36] border-2">
+          <div className="p-2 md:p-4  border-[#e71d36] border-2 h-[100px] md:h-[111px]">
             <div className="mb-2">
               <h1 className="text-xl md:text-2xl">Gmail</h1>
             </div>
             <div>
-              <h1 className="text-[17px] md:text-2xl">Korakodmice@gmail.com</h1>
+              <h1 className="text-[10px] md:text-2xl">Korakodmice@gmail.com</h1>
             </div>
           </div>
-          <div className=" p-2 border-[#e71d36] border-2">
-            <h1 className="text-xl md:text-2xl mb-2">My CV Resume</h1>
-            <button className=" bg-gray-400 p-2 text-xl md:text-2xl">
+          <div className=" p-2 border-[#e71d36] border-2 h-[100px] md:h-[111px]">
+            <h1 className="text-lg md:text-2xl mb-2">My CV Resume</h1>
+            <button className=" bg-gray-400 p-2 text-lg md:text-2xl">
               Resume
             </button>
           </div>
